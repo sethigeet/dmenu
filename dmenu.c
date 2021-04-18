0bc528a3780dddf9199694c5c54b3a95b4f9d760
Geet Sethi <geetsethi05@gmail.com>
2021-04-18 15:24:34 +0530

    :construction: Added a patch to restrict the user from selecting a value that is not there in the list

--- dmenu.c
+++ dmenu.c
@@ -51,6 +51,7 @@ static int dmy = 0; /* put dmenu at this y offset (measured from the bottom if t
 static unsigned int dmw = 0; /* make dmenu this wide */
 static int inputw = 0, promptw, passwd = 0;
 static int lrpad; /* sum of left and right padding */
+static int reject_no_match = 0;
 static size_t cursor;
 static struct item *items = NULL, *backup_items;
 static struct item *matches, *matchend;
@@ -460,12 +461,26 @@ insert(const char *str, ssize_t n)
 {
 	if (strlen(text) + n > sizeof text - 1)
 		return;
+
+	static char last[BUFSIZ] = "";
+	if(reject_no_match) {
+		/* store last text value in case we need to revert it */
+		memcpy(last, text, BUFSIZ);
+	}
+
 	/* move existing text out of the way, insert new text, and update cursor */
 	memmove(&text[cursor + n], &text[cursor], sizeof text - cursor - MAX(n, 0));
 	if (n > 0)
 		memcpy(&text[cursor], str, n);
 	cursor += n;
 	match();
+
+	if(!matches && reject_no_match) {
+		/* revert to last text value if theres no match */
+		memcpy(text, last, BUFSIZ);
+		cursor -= n;
+		match();
+	}
 }
 
 static size_t
@@ -1223,7 +1238,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfivPS] [-l columns] [-l lines] [-h height] [-p prompt] [-fn font] [-m monitor]\n"
+	fputs("usage: dmenu [-bfivPRS] [-l columns] [-l lines] [-h height] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-x xoffset] [-y yoffset] [-z width]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color]\n"
 	      "             [-nhb color] [-nhf color] [-shb color] [-shf color]\n"
@@ -1310,7 +1325,10 @@ main(int argc, char *argv[])
 		else if (!strcmp(argv[i], "-i")) { /* case-insensitive item matching */
 			fstrncmp = strncasecmp;
 			fstrstr = cistrstr;
-		} else if (!strcmp(argv[i], "-P"))   /* is the input a password */
+		}
+		else if (!strcmp(argv[i], "-R")) /* reject input which results in no match */
+			reject_no_match = 1;
+		else if (!strcmp(argv[i], "-P"))   /* is the input a password */
 		        passwd = 1;
 		else if (i + 1 == argc)
 			usage();
